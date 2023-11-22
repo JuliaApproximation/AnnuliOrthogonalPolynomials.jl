da903446f58dc346939af3f83494ffd88e9cb276
Sheehan Olver <solver@mac.com>
2023-11-22 15:46:55 +0000

    ContinuumArrays v0.17 (#7)

--- src/AnnuliOrthogonalPolynomials.jl
+++ src/AnnuliOrthogonalPolynomials.jl
@@ -6,7 +6,7 @@ using BlockArrays, BlockBandedMatrices, ClassicalOrthogonalPolynomials, Continuu
 import Base: in, axes, getindex, broadcasted, tail, +, -, *, /, \, convert, OneTo, show, summary, ==, oneto, diff
 import BlockArrays: block, blockindex, _BlockedUnitRange, blockcolsupport
 import ClassicalOrthogonalPolynomials: checkpoints, ShuffledR2HC, TransformFactorization, ldiv, paddeddata, jacobimatrix, orthogonalityweight, SetindexInterlace
-import ContinuumArrays: @simplify, Weight, weight, grid, ℵ₁, ℵ₀, unweighted, plan_grid_transform
+import ContinuumArrays: @simplify, Weight, weight, grid, ℵ₁, ℵ₀, unweighted, plan_transform
 import HarmonicOrthogonalPolynomials: BivariateOrthogonalPolynomial, MultivariateOrthogonalPolynomial, Plan
 import MultivariateOrthogonalPolynomials: BlockOneTo, ModalInterlace, Laplacian, ModalTrav
 import SemiclassicalOrthogonalPolynomials: divdiff, HalfWeighted
