1fb72c4f717d854696ee9672d8e513fdde2f1e91
Sheehan Olver <solver@mac.com>
2025-02-15 15:34:15 +0000

    Update Versions

--- src/AnnuliOrthogonalPolynomials.jl
+++ src/AnnuliOrthogonalPolynomials.jl
@@ -6,9 +6,9 @@ using BlockArrays, BlockBandedMatrices, ClassicalOrthogonalPolynomials, Continuu
 import Base: in, axes, getindex, broadcasted, tail, +, -, *, /, \, convert, OneTo, show, summary, ==, oneto, diff
 import BlockArrays: block, blockindex, _BlockedUnitRange, blockcolsupport
 import ClassicalOrthogonalPolynomials: checkpoints, ShuffledR2HC, TransformFactorization, ldiv, paddeddata, jacobimatrix, orthogonalityweight, SetindexInterlace
-import ContinuumArrays: @simplify, Weight, weight, grid, ℵ₁, ℵ₀, unweighted, plan_transform
+import ContinuumArrays: Weight, weight, grid, ℵ₁, ℵ₀, unweighted, plan_transform
 import HarmonicOrthogonalPolynomials: BivariateOrthogonalPolynomial, MultivariateOrthogonalPolynomial, Plan
-import MultivariateOrthogonalPolynomials: BlockOneTo, ModalInterlace, Laplacian, ModalTrav
+import MultivariateOrthogonalPolynomials: BlockOneTo, ModalInterlace, laplacian, ModalTrav
 import SemiclassicalOrthogonalPolynomials: divdiff, HalfWeighted
 
 export Block, SVector, CircleCoordinate, ZernikeAnnulus, ComplexZernikeAnnulus, Laplacian
